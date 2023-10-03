fc58fd93134a1ac32d593ef759f2bf5f16efd957
Adithya Adarsh <65715117+AdithyaAdarsh@users.noreply.github.com>
2023-10-03 14:46:31 +0530

    Delete backend directory

--- backend/okta_config.py
+++
@@ -1,8 +0,0 @@
-# okta_config.py
-
-OKTA_CONFIG = {
-    'issuer': 'https://dev-32617710.okta.com/oauth2/default',
-    'client_id': '0oab13mt0oKO5ozPi5d7',
-    'redirect_uri': 'http://localhost:3000/login/callback',  # Adjust as needed
-    'pkce': True,
-}
